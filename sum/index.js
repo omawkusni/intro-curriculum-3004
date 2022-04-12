7d9b6d88dd22aa7f370321c0dab135e179ff8c0d
omaw kusni <t0o0k0o0@hotmail.com>
2022-04-12 22:38:53 +0900

    multi関数の追加

--- sum/index.js
+++ sum/index.js
@@ -3,12 +3,12 @@
 /**
  * 数値の配列を受け取って、その要素の合計を返す関数
  */
-function add(numbers) {
-  let result = 0;
+function multi(numbers) {
+  let result = 1;
   for (const num of numbers) {
-    result = result + num;
+    result = result * num;
   }
   return result;
 }
 
-module.exports = { add };
+module.exports = { multi };
